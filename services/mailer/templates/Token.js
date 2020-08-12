dd95ad3360ee07d1e5f6beb32a884d36cbb37383
kaye <kevinyusyus@gmail.com>
2020-08-12 20:29:59 +0700

    Update Token.js

--- services/mailer/templates/Token.js
+++ services/mailer/templates/Token.js
@@ -135,7 +135,7 @@ exports.TokenTemplate = (token,name,image) => {
 	<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px; font-family: 'Trebuchet MS', Tahoma, sans-serif"><![endif]-->
 	<div style="color:#555555;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
 	<div style="line-height: 1.2; font-size: 12px; color: #555555; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; mso-line-height-alt: 14px;">
-	<p style="font-size: 24px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 29px; margin: 0;"><span style="font-size: 24px;"><strong>2019 UPH Medan Ambassador Voting</strong></span></p>
+	<p style="font-size: 24px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 29px; margin: 0;"><span style="font-size: 24px;"><strong>2020 UPH Medan Ambassador Voting</strong></span></p>
 	</div>
 	</div>
 	<!--[if mso]></td></tr></table><![endif]-->
